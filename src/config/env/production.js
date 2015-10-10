a49c1712df3b325ac8cf0da42c34b19acb6cbfa7
tokarev <eugene.tokarev@gmail.com>
2015-10-10 16:01:05 +0700

    [HF6] added proxyUrl to prod settings

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 80,
-
+  proxyHost: 'http://avaea.com/'
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
