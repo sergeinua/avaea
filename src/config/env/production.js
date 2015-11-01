6dc40070dc261a95fb48e2095aa415100349e9ce
tokarev <eugene.tokarev@gmail.com>
2015-11-02 00:37:03 +0700

    [hf17] changed port to 8080

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 80,
+  port: 8080,
   proxyHost: 'http://ec2-52-24-104-220.us-west-2.compute.amazonaws.com/',
 
   /***************************************************************************
