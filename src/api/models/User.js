fa025da7d61e7d53497c9c16d73a81808a4aea52
tokarev <eugene.tokarev@gmail.com>
2015-11-03 19:26:45 +0700

    [hf22] fixed issue #4; added readable errors

--- src/api/models/User.js
+++ src/api/models/User.js
@@ -1,10 +1,28 @@
+/* global _ */
+/* global sails */
 var User = {
   // Enforce model schema in the case of schemaless databases
   schema: true,
-
+  types: {
+    allowedEmail: function (email) {
+      var whiteList = [
+        "constfilin@gmail.com",
+        "v.mustafin@gmail.com",
+        "eugene.tokarev@gmail.com"
+      ];
+      if (_.indexOf(whiteList, email)) {
+        return true;
+      }
+      if ( /^.+?@avaea.com$/.exec(email) ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
   attributes: {
     username  : { type: 'string', unique: true },
-    email     : { type: 'email',  unique: true /*, regex: '/.+?@avaea.com$/'*/ },
+    email     : { type: 'email',  unique: true, allowedEmail: true },
     passports : { collection: 'Passport', via: 'user' },
     searches  : { collection: 'Search', via: 'user' }
   }
