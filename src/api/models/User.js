d0f1da6884ea7aba215d131d6dee4885b4b216ef
tokarev <eugene.tokarev@gmail.com>
2015-11-03 23:06:39 +0700

    [hf24] email validation refactoring

--- src/api/models/User.js
+++ src/api/models/User.js
@@ -3,26 +3,9 @@
 var User = {
   // Enforce model schema in the case of schemaless databases
   schema: true,
-  types: {
-    allowedEmail: function (email) {
-      var whiteList = [
-        "constfilin@gmail.com",
-        "v.mustafin@gmail.com",
-        "eugene.tokarev@gmail.com"
-      ];
-      if (_.indexOf(whiteList, email)) {
-        return true;
-      }
-      if ( /^.+?@avaea.com$/.exec(email) ) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  },
   attributes: {
     username  : { type: 'string', unique: true },
-    email     : { type: 'email',  unique: true, allowedEmail: true },
+    email     : { type: 'email',  unique: true, regex: /^([^@]+?@avaea.com|constfilin@gmail.com|v.mustafin@gmail.com|eugene.tokarev@gmail.com)$/ },
     passports : { collection: 'Passport', via: 'user' },
     searches  : { collection: 'Search', via: 'user' }
   }
