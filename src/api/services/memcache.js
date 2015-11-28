8124b7125da75498369a4cfb7b6f4b119ee1bd9a
tokarev <eugene.tokarev@gmail.com>
2015-11-28 21:11:17 +0700

    added search result saving to memcache (lost file)

---
+++ src/api/services/memcache.js
@@ -0,0 +1,46 @@
+/* global sails */
+module.exports = {
+
+  client: null,
+
+  init: function () {
+    if (!sails.config.connections.memcacheConf.exptime) {
+      sails.config.connections.memcacheConf.exptime = 0;
+    }
+    if (this.client) {
+      return this.client;
+    }
+    // sails.log(sails.config.connections.memcacheConf);
+    var mc = require('mc');
+    this.client = new mc.Client(
+      sails.config.connections.memcacheConf.host
+      + ':' + sails.config.connections.memcacheConf.port
+    );
+    this.client.connect(function() {
+      sails.log.info("Connected to the memcache on host '"
+        + sails.config.connections.memcacheConf.host + "' on port "
+        + sails.config.connections.memcacheConf.port + "!");
+      // this.client.setAdapter(mc.Adapter.json);
+    });
+  },
+
+  store: function (key, value, callback) {
+    this.init();
+    this.client.set( key, value, { flags: 0, exptime: sails.config.connections.memcacheConf.exptime}, function(err, status) {
+      if (!err) { 
+        console.log(status); // 'STORED' on success!
+        return callback();
+      }
+    });
+  },
+
+  //Get may take a single key, or an array of keys.
+  get: function (key, callback) {
+    this.init();
+    this.client.get( key, function(err, response) {
+      if (!err) {
+        console.log(response[key]);  // should output a simple string.
+      }
+    });
+  }
+}
