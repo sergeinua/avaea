cce7015d5367a65274a9a16605aff0fe18a3307a
tokarev <eugene.tokarev@gmail.com>
2016-01-22 20:03:57 +0700

    [Issue #58] abo redesigned; added spider chat with dummy data

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -4,6 +4,88 @@ $(document).ready(function() {
     return true;
   }
 
+  var dummyData = [];
+  //prefill dummy data
+  for (var i = 1; i <= 100; i++) {
+    dummyData[i] =
+    [
+        {
+            label: "User tile prediction values",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [
+                Math.floor(Math.random() * 100),
+                Math.floor(Math.random() * 100),
+                Math.floor(Math.random() * 100),
+                Math.floor(Math.random() * 100),
+                Math.floor(Math.random() * 100),
+                Math.floor(Math.random() * 100),
+                Math.floor(Math.random() * 100)
+            ]
+        }
+    ];
+  }
+
+  $('.navbar-nav li').click(function() {
+        $('.navbar-nav li').each(function(page) {
+            var t = $(this).find('a').attr('target');
+//            console.log(page);
+            $(this).removeClass('active');
+            $('#'+t).addClass('hidden');
+        });
+        var target = $(this).find('a').attr('target');
+        $(this).addClass('active');
+        console.log(target);
+        $('#' + target).removeClass('hidden');
+        return false;
+    });
+    var substringMatcher = function(strs) {
+        return function findMatches(q, cb) {
+          var matches, substringRegex;
+
+          // an array that will be populated with substring matches
+          matches = [];
+
+          // regex used to determine if a string contains the substring `q`
+          substrRegex = new RegExp(q, 'i');
+
+          // iterate through the pool of strings and for any string that
+          // contains the substring `q`, add it to the `matches` array
+          $.each(strs, function(i, str) {
+            if (substrRegex.test(str)) {
+              matches.push(str);
+            }
+          });
+
+          cb(matches);
+        };
+      };
+      console.log(user_list);
+    $('#user_search_form').typeahead({
+      hint: true,
+//      highlight: true,
+      minLength: 2
+    }, {
+        name: 'users',
+//        limit: 8,
+        source: substringMatcher(user_list),
+        templates: {
+            empty: [
+                '<div class="empty-message">',
+                'unable to find the user that match the current query',
+                '</div>'
+            ].join('\n'),
+            suggestion: function(vars) { return '<div>'+vars + '</div>'; }
+        }
+    });
+
+    
+    
+
   var lastUpdated = 0;
 
   var user_id = 0;
@@ -16,8 +98,8 @@ $(document).ready(function() {
     itinerary_prediction  : {title: 'itinerary prediction', colorClass: 'active'}
   };
   var autoscrollme = function () {
-    $('html, body').animate({ 
-      scrollTop: $(document).height()-$(window).height()}, 
+    $('#log_table_div').animate({ 
+      scrollTop: $('#log_table_div').height()-$(window).height()}, 
       140
     );
   };
@@ -80,9 +162,21 @@ $(document).ready(function() {
 
   setInterval(function() {getLogAction()}, 2000);
 
-  $('.filter_user').click(function() {
+  $('.filter_user').submit(function() {
     $('.alert').remove();
-    user_id = $(this).attr('target');
+    user_id = $('#user_search_form').val();
+    var id = 0;
+    if (id = user_id.match(/^ID#(\d+)/)) {
+      user_id = id[1];
+      radarChartData.datasets = dummyData[user_id];
+      console.log(dummyData[user_id]);
+      window.myRadar = new Chart(document.getElementById("canvas").getContext("2d")).Radar(radarChartData, {
+        responsive: true
+      });  
+    } else {
+        user_id = 0;
+        window.myRadar = null;
+    }
     $('#user_filter_value').val('');
     if (user_id) {
       lastUpdated = 0;
@@ -104,19 +198,35 @@ $(document).ready(function() {
     });
     return true;
   });
-
-    var getSliderSettings = function() {
-        return {
-            dots: true,
-            infinite: false,
-            mobileFirst: true,
-            adaptiveHeight: true,
-            slidesToShow: Math.floor($('body').outerWidth(true)/300)
-        }
+    var radarChartData = {
+        labels: ["Price", "Duration", "Airline", "Outbound Departure", "Outbound Arrival", "Inbound Departure", "Inbound Arrival"],
+        datasets: dummyData[1]
+//        [
+//            {
+//                label: "User tile prediction values",
+//                fillColor: "rgba(220,220,220,0.2)",
+//                strokeColor: "rgba(220,220,220,1)",
+//                pointColor: "rgba(220,220,220,1)",
+//                pointStrokeColor: "#fff",
+//                pointHighlightFill: "#fff",
+//                pointHighlightStroke: "rgba(220,220,220,1)",
+//                data: [65,59,90,81,56,55,40]
+//            }
+//        ]
     };
-    $('#user_tiles').slick(getSliderSettings());
-    $( window ).resize(function() {
-        $('#user_tiles').slick('unslick');
-        $('#user_tiles').slick(getSliderSettings());
-    });
+
+//    var getSliderSettings = function() {
+//        return {
+//            dots: true,
+//            infinite: false,
+//            mobileFirst: true,
+//            adaptiveHeight: true,
+//            slidesToShow: Math.floor($('body').outerWidth(true)/300)
+//        }
+//    };
+//    $('#user_tiles').slick(getSliderSettings());
+//    $( window ).resize(function() {
+//        $('#user_tiles').slick('unslick');
+//        $('#user_tiles').slick(getSliderSettings());
+//    });
 });
