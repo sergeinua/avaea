de550df106a7f5b837bee50c8d017bc98416c3cf
Oleh Shvedov <oshvedov@intropro.com>
2016-02-24 18:41:12 +0200

    DEMO-73, update migration script: set default TRUE for showTiles column

--- src/db/migrations/sql/user_profile.sql
+++ src/db/migrations/sql/user_profile.sql
@@ -1,3 +1,7 @@
 -- Table: profile
 
-ALTER TABLE profile ADD COLUMN "showTiles" boolean;
+ALTER TABLE profile ADD COLUMN "showTiles" boolean DEFAULT true;
+
+-- or
+-- ALTER TABLE profile ALTER COLUMN "showTiles" SET DEFAULT true
+-- if column already exists
