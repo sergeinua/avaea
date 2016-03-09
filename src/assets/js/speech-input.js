63ac955ff6f27873a084fd815d7073381fac96a6
Mikhail <olmiha@gmail.com>
2016-03-09 19:44:02 +0200

    DEMO-92 (No airline popup on Motorola Turbo 1 (Chrome in Android 5.1))

--- src/assets/js/speech-input.js
+++ src/assets/js/speech-input.js
@@ -3,86 +3,91 @@
 (function() {
 	'use strict';
 	window.___gcfg = { lang: 'en' };
-	if (! ('webkitSpeechRecognition' in window) ) {
-		console.error('Not supported!');
-		$('.si-btn', '.voiceSearch').hide();
-		return;
-	}
-
-	var talkMsg = 'Start Talking';
-	var patience = 6;
 
 	function capitalize(str) {
 		return str.length ? str[0].toUpperCase() + str.slice(1) : str;
 	}
 
-	var speechInputWrappers = document.getElementsByClassName('si-wrapper');
+	if (! ('webkitSpeechRecognition' in window) ) {
+		console.log('Not supported!');
+		$('.si-btn', '.voiceSearch').hide();
+		//return;
+	} else {
 
-	[].forEach.call(speechInputWrappers, function(speechInputWrapper) {
-		// find elements
-		var inputEl = speechInputWrapper.querySelector('.si-input');
-		var micBtn = speechInputWrapper.querySelector('.si-btn');
+		var talkMsg = 'Start Talking';
+		var patience = 6;
 
-		// size and position them
-		var inputHeight = inputEl.offsetHeight;
-		var inputRightBorder = parseInt(getComputedStyle(inputEl).borderRightWidth, 10);
-		var buttonSize = 0.8 * inputHeight;
-		micBtn.style.top = 0.1 * inputHeight + 'px';
-		micBtn.style.height = micBtn.style.width = buttonSize + 'px';
-		inputEl.style.paddingRight = buttonSize - inputRightBorder + 'px';
-		speechInputWrapper.appendChild(micBtn);
 
-		// setup recognition
-		var finalTranscript = '';
-		var recognizing = false;
-		var timeout;
-		var oldPlaceholder = null;
-		var recognition = new webkitSpeechRecognition();
-		recognition.continuous = true;
 
-		function restartTimer() {
-			timeout = setTimeout(function() {
-				recognition.stop();
-			}, patience * 1000);
-		}
+		var speechInputWrappers = document.getElementsByClassName('si-wrapper');
 
-		recognition.onstart = function() {
-			oldPlaceholder = inputEl.placeholder;
-			inputEl.placeholder = talkMsg;
-			recognizing = true;
-			micBtn.classList.add('listening');
-			restartTimer();
-		};
+		[].forEach.call(speechInputWrappers, function(speechInputWrapper) {
+			// find elements
+			var inputEl = speechInputWrapper.querySelector('.si-input');
+			var micBtn = speechInputWrapper.querySelector('.si-btn');
 
-		recognition.onend = function() {
-			recognizing = false;
-			clearTimeout(timeout);
-			micBtn.classList.remove('listening');
-			if (oldPlaceholder !== null) inputEl.placeholder = oldPlaceholder;
-		};
+			// size and position them
+			var inputHeight = inputEl.offsetHeight;
+			var inputRightBorder = parseInt(getComputedStyle(inputEl).borderRightWidth, 10);
+			var buttonSize = 0.8 * inputHeight;
+			micBtn.style.top = 0.1 * inputHeight + 'px';
+			micBtn.style.height = micBtn.style.width = buttonSize + 'px';
+			inputEl.style.paddingRight = buttonSize - inputRightBorder + 'px';
+			speechInputWrapper.appendChild(micBtn);
 
-		recognition.onresult = function(event) {
-			clearTimeout(timeout);
-			for (var i = event.resultIndex; i < event.results.length; ++i) {
-				if (event.results[i].isFinal) {
-					finalTranscript += event.results[i][0].transcript;
+			// setup recognition
+			var finalTranscript = '';
+			var recognizing = false;
+			var timeout;
+			var oldPlaceholder = null;
+			var recognition = new webkitSpeechRecognition();
+			recognition.continuous = true;
+
+			function restartTimer() {
+				timeout = setTimeout(function() {
+					recognition.stop();
+				}, patience * 1000);
+			}
+
+			recognition.onstart = function() {
+				oldPlaceholder = inputEl.placeholder;
+				inputEl.placeholder = talkMsg;
+				recognizing = true;
+				micBtn.classList.add('listening');
+				restartTimer();
+			};
+
+			recognition.onend = function() {
+				recognizing = false;
+				clearTimeout(timeout);
+				micBtn.classList.remove('listening');
+				if (oldPlaceholder !== null) inputEl.placeholder = oldPlaceholder;
+			};
+
+			recognition.onresult = function(event) {
+				clearTimeout(timeout);
+				for (var i = event.resultIndex; i < event.results.length; ++i) {
+					if (event.results[i].isFinal) {
+						finalTranscript += event.results[i][0].transcript;
+					}
 				}
-			}
-			finalTranscript = capitalize(finalTranscript);
-			inputEl.value = finalTranscript;
-			restartTimer();
-		};
+				finalTranscript = capitalize(finalTranscript);
+				inputEl.value = finalTranscript;
+				restartTimer();
+			};
 
-		micBtn.addEventListener('click', function(event) {
-			event.preventDefault();
-			if (recognizing) {
-				recognition.stop();
-				return;
-			}
-			inputEl.value = finalTranscript = '';
-			recognition.start();
-		}, false);
-	});
+			micBtn.addEventListener('click', function(event) {
+				event.preventDefault();
+				if (recognizing) {
+					recognition.stop();
+					return;
+				}
+				inputEl.value = finalTranscript = '';
+				recognition.start();
+			}, false);
+		});
+	}
+
 
 	$('#voiceSearch').click(function () {
 		demo();
