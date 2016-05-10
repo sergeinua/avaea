5872c15af4e0dcb8d14ba9b92bb20a88fafaf4d0
Igor A <igorya.inscriptio@gmail.com>
2016-05-10 15:10:32 +0300

    DEMO-287: Fix format ot the profile.birthday field

--- src/api/controllers/UserController.js
+++ src/api/controllers/UserController.js
@@ -39,6 +39,8 @@ module.exports = {
           else
             profile_fields[prop] = found[prop];
         }
+        if(typeof profile_fields.birthday == 'object')
+          profile_fields.birthday = sails.moment(profile_fields.birthday).format('YYYY-MM-DD');
 
         return res.view('user/profile', {
           title:'Update profile',
