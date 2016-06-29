265d05765b7c82d89223f35537f14b3429b6af55
Valentine Kaminskiy <valentine.kaminskiy@gmail.com>
2016-06-29 15:51:48 +0800

    Add migrations to the sandbox

---
+++ images/node/bin/run_migrations.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+file=/usr/var/avaea/migrations.lock
+
+npm i
+
+n=0
+until [ $n -ge 6 ]
+do
+
+    if [ -e "$file" ] ; then
+        node_modules/db-migrate/bin/db-migrate up modify-profile --config database.json -e docker
+        echo "Migrations executed successfully."
+        exit 0
+    else
+        echo "Waiting for postgres..."
+    fi
+
+    n=$[$n+1]
+    sleep 15
+done
+
+echo "Migrations failed by timeout."
+exit 1
+
+
