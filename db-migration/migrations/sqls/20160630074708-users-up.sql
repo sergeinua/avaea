7b4d24e1e01efa89fa48cd300cca525550407393
Oleh Shvedov <oshvedov@intropro.com>
2016-06-30 11:22:46 +0300

    DEMO-45&, add user list to initial migration

---
+++ db-migration/migrations/sqls/20160630074708-users-up.sql
@@ -0,0 +1,35 @@
+BEGIN;
+
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'c.filin@avaea.com', '1', '10', '2015-11-03 17:39:54+00', '2015-11-03 17:39:54+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'constfilin@gmail.com', '1', '11', '2015-11-03 17:40:08+00', '2015-11-03 17:40:08+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'v.mustafin@gmail.com', '1', '12', '2015-11-04 15:37:55+00', '2015-11-04 15:37:55+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'cc@avaea.com', '1', '13', '2015-11-05 02:39:21+00', '2015-11-05 02:39:21+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'igor.markov1@gmail.com', '1', '14', '2015-11-05 03:30:04+00', '2015-11-05 03:30:04+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'e.tokarev@avaea.com', '1', '15', '2015-11-05 11:07:09+00', '2015-11-05 11:07:09+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'jafri@avaea.com', '1', '16', '2015-11-14 23:21:46+00', '2015-11-14 23:21:46+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'm.mccabe@avaea.com', '1', '18', '2016-01-14 06:15:14+00', '2016-01-14 06:15:14+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'germiy@gmail.com', '1', '19', '2016-01-14 19:16:43+00', '2016-01-14 19:16:43+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'igorya.inscriptio@gmail.com', '1', '22', '2016-02-17 15:32:18+00', '2016-02-17 15:32:18+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'valentine.kaminskiy@gmail.com', '1', '23', '2016-03-05 16:49:05+00', '2016-03-05 16:49:05+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'olmiha@gmail.com', '1', '24', '2016-03-06 10:39:45+00', '2016-03-06 10:39:45+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'wkspilman@gmail.com', '1', '25', '2016-03-09 23:38:34+00', '2016-03-09 23:38:34+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'a.yaqub@avaea.com', '1', '26', '2016-03-10 06:04:06+00', '2016-03-10 06:04:06+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'constfilin@upwork.com', '0', '27', '2016-03-15 06:07:39+00', '2016-03-15 06:07:39+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'igorya105@gmail.com', '1', '28', '2016-03-16 22:16:35+00', '2016-03-16 22:16:35+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  '3susaimmigration@gmail.com', '1', '30', '2016-03-31 18:18:17+00', '2016-03-31 18:18:17+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'wwalker4u@gmail.com', '0', '31', '2016-04-02 13:42:49+00', '2016-04-02 13:42:49+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'atsyplikhin@gmail.com', '0', '32', '2016-04-19 17:48:47+00', '2016-04-19 17:48:47+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'lifestreampartners@gmail.com', '0', '33', '2016-04-23 03:17:55+00', '2016-04-23 03:17:55+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'mvyhouski@gmail.com', '0', '29', '2016-03-21 20:25:54+00', '2016-03-21 20:25:54+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'manoj.mystifly@gmail.com', '0', '20', '2016-01-25 08:16:30+00', '2016-01-25 08:16:30+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'scottinsfbay@gmail.com', '0', '17', '2015-12-27 01:35:01+00', '2015-12-27 01:35:01+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'dreg.box@gmail.com', '1', '34', '2016-04-25 09:07:38+00', '2016-04-25 09:07:38+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'eugene.tokarev@gmail.com', '0', '35', '2016-04-27 17:42:00+00', '2016-04-27 17:42:00+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'someone97531@gmail.com', '0', '36', '2016-05-03 19:11:37+00', '2016-05-03 19:11:37+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  '371221zp@gmail.com', '0', '37', '2016-05-14 18:31:09+00', '2016-05-14 18:31:09+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'bezpekaaa@gmail.com', '0', '21', '2016-02-04 08:14:43+00', '2016-02-04 08:14:43+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'tuenguyen.vec@gmail.com', '1', '38', '2016-05-26 03:04:19+00', '2016-05-26 03:04:19+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'ksusha.oliynik@gmail.com', '0', '39', '2016-06-05 16:48:13+00', '2016-06-05 16:48:13+00');
+insert into "user" (username, email, is_whitelist, id, "createdAt", "updatedAt")  values (null,  'nechaiolya1@gmail.com', '1', '40', '2016-06-13 15:43:00+00', '2016-06-13 15:43:00+00');
+
+COMMIT;
