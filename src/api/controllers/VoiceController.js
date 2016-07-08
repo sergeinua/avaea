125e85590c1f4f1134e11896c1b40bd843fc0b6d
Vlad Mustafin <v.mustafin@gmail.com>
2016-07-08 17:46:26 +0300

    DEMO-447: Voice search WEBAPI

---
+++ src/api/controllers/VoiceController.js
@@ -0,0 +1,24 @@
+/**
+ * VoiceController
+ *
+ * @description :: Server-side api of voice search
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  parse: function (req, res) {
+    var _query = _.trim(req.param('q'));
+    return AvaeaTextParser.run(_query, function(err, result) {
+      if (err) {
+        sails.log.error(err);
+        return res.serverError(); //500
+      }
+      if (req.wantsJSON) {
+        return res.json(result); //200
+      } else {
+        return res.notFound(); //404
+      }
+    });
+  }
+};
+
