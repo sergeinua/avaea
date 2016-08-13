ed261efb6a747321be3bf1c7a03a19d752dc7e9e
Constantine Filin <c.filin@avaea.com>
2016-08-13 00:00:59 -0700

    DEMO-555: removing commented out code

--- src/assets/js/search/form.js
+++ src/assets/js/search/form.js
@@ -333,8 +333,6 @@ $(document).ready(function() {
       return false;
     }
 
-    //var voiceSearchQuery = $.trim($('#voiceSearchTextarea').val()) || '';
-    //$('#voiceSearchQuery').val(voiceSearchQuery);
     $("#searchBanner").modal();
     $('#search_form').attr('action', '/result?s=' + btoa(JSON.stringify($( this ).serializeArray())));
 
