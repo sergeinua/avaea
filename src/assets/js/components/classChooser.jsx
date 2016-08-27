45c12d9cd358630b0e3fc3c3b5aea38f1d0fddec
deborah <deborahsgithub@hotmail.com>
2016-08-26 22:07:49 -0700

    more desktop

--- src/assets/js/components/classChooser.jsx
+++ src/assets/js/components/classChooser.jsx
@@ -2,7 +2,7 @@ class ClassChooser extends React.Component {
   render() {
     return (
       <div className="flight-class-info-item row">
-        <div className="label-d col-xs-6">class</div>
+        <div className="label-d col-xs-6">Class</div>
         <div className="text-picker col-xs-6">{serviceClass[this.props.classVal]}</div>
       </div>
     )
