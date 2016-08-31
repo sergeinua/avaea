29bedb4976258a9c884ebb2060b95bc0d2d16be7
Eugene Tokarev <eugene.tokarev@gmail.com>
2016-08-31 21:55:30 +0700

    DEMO-559 fixed problems with swiping after hide/show tiles

--- src/assets/js/components/buckets/Buckets.jsx
+++ src/assets/js/components/buckets/Buckets.jsx
@@ -26,6 +26,7 @@ var Buckets = React.createClass({
 
   toggleFullInfo: function () {
     return function() {
+      $('body').removeClass('show-tiles-arrow');
       var newVal = !this.state.fullinfo;
       this.setState({fullinfo: newVal});
     }.bind(this);
@@ -34,17 +35,17 @@ var Buckets = React.createClass({
   render: function() {
     return (
     <div>
-      { this.state.fullinfo ?
-        <div className="swiper-container">
-          <div id="tiles" className="swiper-wrapper" data-max_filter_items="{ this.props.max_filter_items }">
+
+        <div className={this.state.fullinfo ? "swiper-container":"swiper-container hidden"}>
+          <div id="tiles" className="swiper-wrapper" data-max_filter_items={ this.props.max_filter_items }>
             {this.state.tiles.map(function (tile) {
               return <Tile key={tile.id} info={tile} />
             })}
           </div>
           <div className="clickable-tiles-area clickable hidden"></div>
           <div className="clickable-tiles-area-yellow clickable"></div>
-        </div>: ''
-      }
+        </div>
+
       <div className="bottomNav">
         <div className="bottom-nav-text">
         <span>Showing <span className='search_count'><span id='search_count'>{ this.props.searchResultLength }</span>/{this.state.searchResultLength} </span></span>
