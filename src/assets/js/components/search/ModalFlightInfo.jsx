52b42d88c240701636c6b61d26a2c33c4798e643
deborah <deborahsgithub@hotmail.com>
2016-09-03 22:52:15 -0700

    flight extras modal finished

--- src/assets/js/components/search/ModalFlightInfo.jsx
+++ src/assets/js/components/search/ModalFlightInfo.jsx
@@ -4,40 +4,43 @@ var ModalFlightInfo = React.createClass({
   },
   render() {
     return (
-      <div data-id={this.props.id} className="modal modal-flight-info-block" role="dialog">
+      <div data-id={this.props.id} className="modal flight-extras" role="dialog">
         <div className="modal-dialog">
           <div className="modal-content">
-            <div className="modal-body">
-            
+          
               <div className="title-bar">
-                <div className="title">Extras</div>
                 <div className="close-x" onClick={this.closeModal()}></div>
+                <div className="title">Extras</div>
               </div>
   
-                 {this.props.info.information.map(function (item, i) {
-                    return <div className="extras" key={"fl-info-" + i}>
-                        <div className="name">{item.name}</div>
-                        <div className="value">{'$'+item.price.toFixed(2) }</div>
+              <div className="modal-body">
+              
+                   {this.props.info.information.map(function (item, i) {
+                      return <div className="extras" key={"fl-info-" + i}>
+                          <div className="name">{item.name}</div>
+                          <div className="value">{'$'+item.price.toFixed(2) }</div>
+                        </div>
+                   })}
+                   
+                   
+                    <div className="total">
+                      <div className="value-from">Extra value from</div>
+                        <div className="wrapper">
+                          <div className="airline">
+                            {this.props.info.citypairs[0].from.airline}
+                          </div>  
+                          <div className="total-value">
+                            {'$' + this.props.info.additionalPrice}
+                          </div>
                       </div>
-                 })}
-                 
-                 
-                <div className="row second row what is it">
-                  <div className="total-title">
-                    {'Extra value from ' + this.props.info.citypairs[0].from.airline}
-                  </div>
-                  <div className="total-value">
-                    {'$' + this.props.info.additionalPrice}
-                  </div>
+                    </div>
+                  
                 </div>
               
-                
                 <div className="buttons-bar">
-                  <button type="button" className="big-button.secondary" onClick={this.closeModal()}>Ok!</button>
+                  <button type="button" className="single big-button secondary" onClick={this.closeModal()}>Ok</button>
                 </div>
-                
-                
-              </div>
+              
             </div>
           </div>
         </div>
