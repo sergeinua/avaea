54b9430f08274582b13ced094705913e06391624
deborah <deborahsgithub@hotmail.com>
2016-09-03 20:52:52 -0700

    incremental - flight extras modal

--- src/assets/js/components/search/ModalFlightInfo.jsx
+++ src/assets/js/components/search/ModalFlightInfo.jsx
@@ -8,26 +8,31 @@ var ModalFlightInfo = React.createClass({
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-body">
-
-                {this.props.info.information.map(function (item, i) {
-                  return <div className="extras" key={"fl-info-" + i}>
-                      <div className="name">{item.name}</div>
-                      <div className="value">{'$'+item.price.toFixed(2) }</div>
-                    </div>
-               })}
-               
-               
-              <div className="row second row what is it">
-                <div className="total-title">
-                  {'Extra value from ' + this.props.info.citypairs[0].from.airline}
-                </div>
-                <div className="total-value">
-                  {'$' + this.props.info.additionalPrice}
-                </div>
+            
+              <div className="title-bar">
+                <div className="title">Extras</div>
+                <div className="close-x" onClick={this.closeModal()}></div>
               </div>
+  
+                 {this.props.info.information.map(function (item, i) {
+                    return <div className="extras" key={"fl-info-" + i}>
+                        <div className="name">{item.name}</div>
+                        <div className="value">{'$'+item.price.toFixed(2) }</div>
+                      </div>
+                 })}
+                 
+                 
+                <div className="row second row what is it">
+                  <div className="total-title">
+                    {'Extra value from ' + this.props.info.citypairs[0].from.airline}
+                  </div>
+                  <div className="total-value">
+                    {'$' + this.props.info.additionalPrice}
+                  </div>
+                </div>
               
                 
-                <div className="buttons">
+                <div className="buttons-bar">
                   <button type="button" className="big-button.secondary" onClick={this.closeModal()}>Ok!</button>
                 </div>
                 
