441f98ffa2f2ce32f4278b6dfd1efdf046ed122b
deborah <deborahsgithub@hotmail.com>
2016-09-13 03:11:59 -0700

    change the div

--- src/assets/js/components/pages/booking.jsx
+++ src/assets/js/components/pages/booking.jsx
@@ -5,8 +5,8 @@
 $(document).ready(function() {
 	
 	//this places the React flight unit
-  if (typeof ItineraryData != 'undefined' && $('#booking-flight-unit').length) {
-      ReactContentRenderer.render(<ResultItem itinerary={ItineraryData}/>, $('#booking-flight-unit'));
+  if (typeof ItineraryData != 'undefined' && $('#booked-flight-unit').length) {
+      ReactContentRenderer.render(<ResultItem itinerary={ItineraryData}/>, $('#booked-flight-unit'));
   }
   
 });
