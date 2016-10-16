66bd7cc8f6725ad2a15260ccec643f4f1e38367a
Eugene Tokarev <eugene.tokarev@gmail.com>
2016-10-16 16:17:05 +0700

    DEMO-674 added alert for non Chrome browsers

--- src/api/policies/passport.js
+++ src/api/policies/passport.js
@@ -25,7 +25,9 @@ module.exports = function (req, res, next) {
   // Initialize Passport
   passport.initialize()(req, res, function () {
     var ua = req.get('user-agent');
+    console.log('User Agent:', ua);
     req.isMobile = res.locals.isMobile = /mobile/i.test(ua);
+    req.showUnsupportedAlert = res.locals.showUnsupportedAlert = !/Chrome/i.test(ua);
     req.mobileDevice = res.locals.mobileDevice = null;
     if (req.isMobile) {
       if (/iPhone|iPad|iPod/i.test(ua)) {
