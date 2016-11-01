dbc52224e2c8ba34a3dece1bb5798db367ceb032
Igor A <igorya.inscriptio@gmail.com>
2016-11-01 23:18:50 +0200

    DEMO-734  Change text style to original in NotFound component

--- src/assets/js/components/search/NotFound.jsx
+++ src/assets/js/components/search/NotFound.jsx
@@ -10,7 +10,7 @@ var NotFound = React.createClass({
     return (
       <div className="nothing-found">
           <div className="copy">
-            {user_messages.map((message, index) => <p key={'msg_'+index}>{message}</p>)}
+            {user_messages.map((message, index) => <div key={'msg_'+index}>{message}</div>)}
           </div>
           <div className="buttons">
             <a href="/search" className="big-button new-search-button" role="button">Try Again</a>
