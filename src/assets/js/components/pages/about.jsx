01a990894de5bd42d77cd265964fd71284b12942
Mykola Boiko <mboiko@intropro.com>
2016-11-06 20:47:04 +0200

    DEMO-714 Move avaea/src/views/site/about.ejs to React

---
+++ src/assets/js/components/pages/about.jsx
@@ -0,0 +1,42 @@
+var AboutPage = React.createClass({
+  getInitialState: function() {
+    return {
+      cur_year: this.props.AboutPageData.cur_year,
+      software_version: this.props.AboutPageData.software_version,
+      contact_email: this.props.AboutPageData.contact_email
+    };
+  },
+
+  render: function() {
+    return (
+      <div className="sitepage about">
+        <div className="copyright">&copy;{this.state.cur_year} AVAEA Inc., All Rights Reserved</div>
+
+        <div className="release-version">
+          Avaea Agent { this.state.software_version}
+        </div>
+
+        <div className="clickmail">
+          <a className="big-button" href={ "mailto:" + this.state.contact_email }>
+            <span className="fa fa-envelope-o"></span>
+            Contact Us
+          </a>
+        </div>
+
+      </div>
+
+    )
+  }
+});
+
+function renderAboutPage(AboutPageData) {
+  if ($('#aboutpage').length) {
+    ReactContentRenderer.render(<AboutPage AboutPageData={AboutPageData}/>, $('#aboutpage'));
+  }
+}
+
+$(document).ready(function() {
+  if (typeof AboutPageData != 'undefined') {
+    renderAboutPage(AboutPageData);
+  }
+});
