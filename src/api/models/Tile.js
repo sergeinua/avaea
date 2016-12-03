b6c42a251bfd712b9f5a198361de1b22f28c7b8b
avaea <cc@avaea.com>
2016-12-03 02:05:25 -0800

    removing unneeded comments

--- src/api/models/Tile.js
+++ src/api/models/Tile.js
@@ -571,20 +571,12 @@ module.exports = {
             sails.log.info('Scenario 6 : Sort while emphasizing preferred airlines');
             cicstanford.compute_departure_times_in_minutes(itineraries);
             cicstanford.determine_airline(itineraries);
-            //cicstanford.print_many_itineraries(itineraries);
             var temp_itins = cicstanford.sort_by_preferred_airlines(itineraries, Tile.userPreferredAirlines);
-            //cicstanford.print_many_itineraries(temp_itins);
           } else {
             cicstanford.compute_departure_times_in_minutes(itineraries);
             cicstanford.determine_airline(itineraries);
-            //cicstanford.print_many_itineraries(itineraries);
-            //sails.log.info('Scenario 6 : Sort while emphasizing preferred airlines');
-            //var temp_itins = cicstanford.sort_by_preferred_airlines(itineraries, Tile.userPreferredAirlines);
-            //cicstanford.print_many_itineraries(itineraries);
             sails.log.info('Scenario 7 : Sort in price and number of stops while emphasizing preferred airlines');
-            //var temp_itins = cicstanford.rank_itineraries_in_3D_by_price_duration_airline2(itineraries, tileArr['Price'].order, tileArr['Price'].order, tileArr['Price'].order, Tile.userPreferredAirlines);
             var temp_itins = cicstanford.rank_itineraries_in_3D_by_price_duration_airline2(itineraries, 1, 1, 1, Tile.userPreferredAirlines);
-            //cicstanford.print_many_itineraries(temp_itins);
           }
       // append the default zero smartRank
       for (var i = 0; i < itineraries.length; i++) {
@@ -613,12 +605,6 @@ module.exports = {
       itineraries = tmp;
       sails.log.info('after DEMO-285', itineraries.length);
     }
-    ////////////////////////////////////////////////////////////////////////////
-    //cicstanford.print_many_itineraries(itineraries);
-    //cicstanford.append_1D_airline_rank2(itineraries, Tile.userPreferredAirlines, tileArr['Price'].order, tileArr['Price'].order);
-    //cicstanford.rank_itineraries_in_3D_by_price_duration_airline2(itineraries, tileArr['Price'].order, tileArr['Price'].order, tileArr['Price'].order, Tile.userPreferredAirlines);
-    //cicstanford.print_many_itineraries(itineraries);
-    ////////////////////////////////////////////////////////////////////////////
 
     sails.log.info('Smart Ranking time: %s', utils.timeLogGetHr('smart_ranking'));
     /* }}} Smart Ranking */
