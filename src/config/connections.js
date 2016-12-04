19a7557ee713b880ee3069fd476e4886c59f323f
Dmitry Bratskiy <dmitrybratskyi@gmail.com>
2016-12-04 16:17:07 +0000

    connections.js edited online with Bitbucket

--- src/config/connections.js
+++ src/config/connections.js
@@ -46,7 +46,7 @@ module.exports.connections = {
     host: '127.0.0.1',
     user: 'avaea',
     password: 'avaea',
-    database: 'avaea_db'
+    database: 'avaea'
   },
   productionPostgresqlServer: {
     adapter: 'sails-postgresql',
