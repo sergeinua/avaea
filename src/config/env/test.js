5c1c82e51d0fa7a73e73b35b012db54814c140a1
Dmitry Bratskiy <dmitrybratskyi@gmail.com>
2016-12-04 05:47:42 +0000

    test.js edited online with Bitbucket

--- src/config/env/test.js
+++ src/config/env/test.js
@@ -22,6 +22,29 @@ module.exports = {
     database: 'avaea_db',
     port: 5432
   },
+flightapis: {
+//    mondee: {
+//      baseEndPoint: 'http://localhost:23456/api/v2'/*, // 'http://sandbox.trippro.com/api/v2',
+//      clientId: 'CFS1017' // CFS login is not actual, CFP login is now used for both PROD and STAGE Mondee API*/
+//    },
+    farelogix: {
+      post_options: {
+        host: 'stg.farelogix.com',
+        path: '/xmlts/sandboxdm'
+      },
+      tc: {
+        iden: {
+          u: "FLXtest",
+          p: "dLKx6Xne",
+          pseudocity: "AEO2",
+          agtpwd: "3l912O8X$p",
+          agy: "05600044"
+        },
+        trace: 'xmlava001'
+      }
+    }
+  },
+
 
 
   hookTimeout: 120000,
