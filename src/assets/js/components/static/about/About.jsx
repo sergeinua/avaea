de73997edff3518ff97dd1f57a9047380f0bac95
deborah <deborahsgithub@hotmail.com>
2016-12-07 04:52:37 -0800

    incremental - about page

--- src/assets/js/components/static/about/About.jsx
+++ src/assets/js/components/static/about/About.jsx
@@ -4,8 +4,163 @@ var StaticAbout = React.createClass({
     return (
       <div>
 	      <div className="content static about">
-	      	<h1 className="skeleton">About Content</h1>
-      	</div>
+	      	<div className="about top level">
+	      		<div className="wrapper">
+		      		<h1>About Us</h1>
+		      		<div className="copy">
+		      			Our AI-powered travel app sorts through thousands of choices 
+		      			to find the choices that are best for you. By applying your 
+		      			preferences and comparing everything a flight has to offer, 
+		      			we make picking the right flight a snap.
+		      		</div>
+		      		<a className="buttonly" href="#">Try a flight search</a>
+	      		</div>
+	      	</div> {/* ends about top */}
+	      	
+	      	<div className="about second level">
+	      		<h2>Who We Are</h2>
+	      		
+	      		<div className="wrapper">
+	      		
+		      		<div className="person jafri">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name">Vajid Jafri</div>
+		      				<div className="ti">CEO</div>
+		      				<div className="bio">
+		      					Proven travel start-up leader and serial entrepreneur. 
+			      				Founder and CEO of cFares and Mondee.
+		      				</div>
+		      			</div>
+		      		</div>
+		      		
+		      		<div className="person generic">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name">Name</div>
+		      				<div className="ti">Title</div>
+		      				<div className="bio">
+		      					Short impressive bio sentence here. Maybe two.
+		      				</div>
+		      			</div>
+		      		</div>
+		      		
+		      		<div className="person generic">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name">Name</div>
+		      				<div className="ti">Title</div>
+		      				<div className="bio">
+		      					Short impressive bio sentence here. Maybe two.
+		      				</div>
+		      			</div>
+		      		</div>
+		      		
+		      		<div className="person generic">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name">Name</div>
+		      				<div className="ti">Title</div>
+		      				<div className="bio">
+		      					Short impressive bio sentence here. Maybe two.
+		      				</div>
+		      			</div>
+		      		</div>
+		      		
+		      		<div className="person generic">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name">Name</div>
+		      				<div className="ti">Title</div>
+		      				<div className="bio">
+		      					Short impressive bio sentence here. Maybe two.
+		      				</div>
+		      			</div>
+		      		</div>
+		      		
+		      		<div className="person generic">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name">Name</div>
+		      				<div className="ti">Title</div>
+		      				<div className="bio">
+		      					Short impressive bio sentence here. Maybe two.
+		      				</div>
+		      			</div>
+		      		</div>
+		      		
+		      		<div className="person generic">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name">Name</div>
+		      				<div className="ti">Title</div>
+		      				<div className="bio">
+		      					Short impressive bio sentence here. Maybe two.
+		      				</div>
+		      			</div>
+		      		</div>
+		      		
+		      		<div className="person generic">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name">Name</div>
+		      				<div className="ti">Title</div>
+		      				<div className="bio">
+		      					Short impressive bio sentence here. Maybe two.
+		      				</div>
+		      			</div>
+		      		</div>
+		      		
+		      		<div className="person generic">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name">Name</div>
+		      				<div className="ti">Title</div>
+		      				<div className="bio">
+		      					Short impressive bio sentence here. Maybe two.
+		      				</div>
+		      			</div>
+		      		</div>
+		      		
+		      		<div className="person generic">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name">Name</div>
+		      				<div className="ti">Title</div>
+		      				<div className="bio">
+		      					Short impressive bio sentence here. Maybe two.
+		      				</div>
+		      			</div>
+		      		</div>
+		      		
+		      		<div className="person generic">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name">Name</div>
+		      				<div className="ti">Title</div>
+		      				<div className="bio">
+		      					Short impressive bio sentence here. Maybe two.
+		      				</div>
+		      			</div>
+		      		</div>
+		      		
+		      		<div className="person blank">
+		      			<div className="image"></div>
+		      			<div className="holder">
+		      				<div className="name"> </div>
+		      				<div className="ti"> </div>
+		      				<div className="bio"></div>
+		      			</div>
+		      		</div>
+	      		
+	      		</div> {/* ends team wrapper */}
+	      	</div> {/* ends second level */}
+	      	
+	      	<div className="button-holder">
+	      		<a className="buttonly" href="#">Contact us</a>
+	      	</div>
+	      
+      	</div> {/* ends content */}
       </div>
     )
   }
