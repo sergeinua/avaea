28fd7e5296ff142761cc7a28c2c9dd6d1eccbf8b
deborah <deborahsgithub@hotmail.com>
2016-12-06 20:15:19 -0800

    incremental - home level 2

--- src/assets/js/components/static/home/Home.jsx
+++ src/assets/js/components/static/home/Home.jsx
@@ -4,7 +4,7 @@ var StaticHome = React.createClass({
     		<div>
 	    		<div className="content static home">
 	
-	    	  	<div className="home level top">
+	    	  	<div className="home top level">
 	    	  
 		    	    <div className="video-holder">   
 		    	      <video poster="../images/static/tarmac-placeholder.jpg" id="bgvid" playsInline autoPlay muted loop>
@@ -33,21 +33,61 @@ var StaticHome = React.createClass({
 				    	    	</div>
 			    	    	  <a className="buttonly" href="#">Try it</a>
 				    	    </form>
-			    	    </div>{/* ends try-it form */}
+			    	    </div>{/* ends try-it form */} 
 			    	    
 			    	  </div> {/* ends home-top-copy */}
-			    	    
-		    	  </div> {/* ends home level top */}
+		    	  </div> {/* ends home top level */}
 		    	  
-		    	  <div className="home level second">
-			  	  	<p>Here is home second level content.</p>
-			  	  </div>
 		    	  
-		    	  <div className="home level third">
+		    	  <div className="home second level">
+			    	  <h1>It's time technology made booking travel easier.</h1>
+			    	  <div className="static-features">
+			    	  
+			    	  	<div className="feature-voice">
+			    	  		<h2>Talk to us</h2>
+			    	  		<h3>when you're on the go</h3>
+			    	  		<div className="graphic"></div>
+			    	  		<div className="copy">
+				    	  		<span>Book your travel using your voice instead of your thumbs. </span> 
+				    	  		Walk, talk, and get your tickets. Our voice agent hears and 
+				    	  		searches - you don’t need to touch the keyboard.
+			    	  		</div>
+				    	  </div>
+				    	  
+				    	  <div className="feature-ff">
+			    	  		<h2>See FF Miles</h2>
+			    	  		<h3>up front value</h3>
+			    	  		<div className="graphic"></div>
+			    	  		<div className="copy">
+				    	  		<span>We show you what matters to you. </span> 
+				    	  		Like how many Frequent Flyer miles each ticket awards. 
+				    	  		And, we make it easy to weigh options like price and 
+				    	  		duration against airlines, scheduling 
+				    	  		and wireless availability. 
+			    	  		</div>
+				    	  </div>
+				    	  
+				    	  <div className="feature-price">
+			    	  		<h2>Low Price</h2>
+			    	  		<h3>try it out</h3>
+			    	  		<div className="graphic"></div>
+			    	  		<div className="copy">
+				    	  		<span>Your wallet will love our prices. </span> 
+				    	  		Your free time will love how much faster it is to 
+				    	  		identify the ticket you wanted without 
+				    	  		requiring a Ph.D in filtering. Booking is easy now.
+			    	  		</div>
+				    	  </div>
+				    	  
+				    	  
+				    	</div>{/* ends features */}  
+		    	  </div> {/* ends home second level */}
+		    	  
+		    	  <div className="home third level">
 			  	  	<p>Here is home third level content.</p>
 			  	  </div>
 			  	
-		    	  <div className="home level bottom">
+		    	  <div className="home bottom level">
 		    	  	<p>Here is home bottom level content.</p>
 		    	  </div>
 		    	  
