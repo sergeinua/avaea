bab875056906528e3b63925d259cca2ad344379d
Dmitry Bratskiy <dmitrybratskyi@gmail.com>
2016-12-13 09:51:26 +0000

    bootstrap.unit.test.js created online with Bitbucket

---
+++ src/tests/bootstrap.unit.test.js
@@ -0,0 +1,47 @@
+var Sails = require('sails'),
+  sails;
+  require('should');
+
+before(function(done) {
+  // Increase the Mocha timeout so that Sails has enough time to lift.
+  // this.timeout(10000);
+  // timeout increased to let Pipelines work
+    this.timeout(512000);
+
+  Sails.lift({
+    // configuration for testing purposes
+    environment: 'test',
+    port: 4000,
+    proxyHost: 'http://localhost/',
+    proxyPort: 4000,
+    explicitHost: false,
+    models: {
+      connection: 'testMemoryDb'
+    },
+    log: {
+      level: 'error',
+      timestamp: false
+    },
+    session: false,
+    policies: {
+      '*' : true
+    },
+   // hookTimeout: 40000,
+   // timeout increased to let Pipelines work
+    hookTimeout: 512000,
+    hooks: {
+      session: false,
+      grunt: false
+    },
+  }, function(err, server) {
+    sails = server;
+    if (err) return done(err);
+    // here you can load fixtures, etc.
+    done(err, sails);
+  });
+});
+
+after(function(done) {
+  // here you can clear fixtures, etc.
+  Sails.lower(done);
+});
