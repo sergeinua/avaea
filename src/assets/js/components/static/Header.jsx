6db3881ee5cce9d0896a0de8a79feee492947a93
deborah <deborahsgithub@hotmail.com>
2016-12-14 05:40:53 -0800

    home responsive base complete

--- src/assets/js/components/static/Header.jsx
+++ src/assets/js/components/static/Header.jsx
@@ -3,6 +3,10 @@ var StaticHeader = React.createClass({
 onItemClick: function (event) {
 	$("header.static nav").toggle();
 },
+
+onPageClick: function (event) {
+	$("header.static nav").hide();
+},
 	
   render: function () {
     return (
@@ -11,11 +15,11 @@ onItemClick: function (event) {
         <nav>
         	<div className="left">
 	        <ul className="menu nav">
-		        <li className={this.props.location.pathname == '/home'? 'active':''}><Link to="/home">Home</Link></li>
-		        <li className={this.props.location.pathname == '/about'? 'active':''}><Link to="/about">About</Link></li>
-		        <li className={this.props.location.pathname == '/news'? 'active':''}><Link to="/news">News</Link></li>
-		        <li className={this.props.location.pathname == '/partner'? 'active':''}><Link to="/partner">Partner</Link></li>
-		        <li className={this.props.location.pathname == '/contact'? 'active':''}><Link to="/contact">Contact</Link></li>
+		        <li className={this.props.location.pathname == '/home'? 'active':''}><Link to="/home" onClick={this.onItemClick}>Home</Link></li>
+		        <li className={this.props.location.pathname == '/about'? 'active':''}><Link to="/about" onClick={this.onItemClick}>About</Link></li>
+		        <li className={this.props.location.pathname == '/news'? 'active':''}><Link to="/news" onClick={this.onItemClick}>News</Link></li>
+		        <li className={this.props.location.pathname == '/partner'? 'active':''}><Link to="/partner" onClick={this.onItemClick}>Partner</Link></li>
+		        <li className={this.props.location.pathname == '/contact'? 'active':''}><Link to="/contact" onClick={this.onItemClick}>Contact</Link></li>
 		      </ul>
 		      </div>
 		      
@@ -23,8 +27,8 @@ onItemClick: function (event) {
 		      <ul className="menu user"> 
 		        {this.props.user.email ?
 		          <span>
-		            <li className="flight-search"><Link to="/search" className="flight-search">Flight Search</Link></li>
-		            <li className="active"> <a href="/logout">Log out</a></li>
+		            <li className="flight-search"><Link to="/search" className="flight-search" onClick={this.onItemClick}>Flight Search</Link></li>
+		            <li className="active"> <a href="/logout" onClick={this.onItemClick}>Log out</a></li>
 		          </span>
 		          :
 		          <li className="active"><a href="/login">Login</a></li>
