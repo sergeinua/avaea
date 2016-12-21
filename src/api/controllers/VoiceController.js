51559f99cef5f9a497866f0fd490c337214189cf
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2016-12-21 19:43:54 +0100

    DEMO-855: 'parseApiAi' router, ApiAiParser.js

--- src/api/controllers/VoiceController.js
+++ src/api/controllers/VoiceController.js
@@ -6,6 +6,7 @@
  */
 
 var _ = require('lodash');
+var ApiAiParser = require('./ApiAiParser.js');
 
 module.exports = {
   parse: function (req, res) {
@@ -23,6 +24,19 @@ module.exports = {
       } else {
         return res.notFound(); //404
       }
+    });
+  },
+
+  parseApiAi: function (req, res) {
+    var _query = _.trim(req.param('q'));
+    new ApiAiParser().parse(_query, req.sessionId).then(function(result){
+    if (req.wantsJSON) {
+      if (sails.config.environment !== 'test')
+        segmentio.track(req.user.id, 'Voice Search', {query: _query, result: result});
+      return res.json(result); //200
+    } else {
+      return res.notFound(); //404
+    }
     });
   },
 
