be31c6ea3e764bf437b0151aebc388d7f4072878
deborah <deborahsgithub@hotmail.com>
2016-12-31 04:20:45 -0800

    ONV-870 restore config files committed accidentally

--- src/config/env/development.js
+++ src/config/env/development.js
@@ -25,8 +25,10 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  proxyHost: 'http://localhost',
-  proxyPort: 1337,
+  port: 80,
+  proxyHost: 'http://stage.avaea.com/',
+  proxyPort: 80,
+  explicitHost: 'localhost',
 
 
   /***************************************************************************
@@ -49,7 +51,7 @@ module.exports = {
   flightapis: {
     searchProvider: ['mondee', 'farelogix'/*, 'mystifly'*/],
     mondee: {
-    	baseEndPoint: 'http://localhost:34567/api/v2'/*, // 'http://sandbox.trippro.com/api/v2',
+    	baseEndPoint: 'http://localhost:23456/api/v2'/*, // 'http://sandbox.trippro.com/api/v2',
       clientId: 'CFS1017' // CFS login is not actual, CFP login is now used for both PROD and STAGE Mondee API*/
     },
     farelogix: {
