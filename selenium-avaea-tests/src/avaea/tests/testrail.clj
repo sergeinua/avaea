5bb5f4243a5b25093c517996d0506e91fca7d00d
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2017-01-11 23:03:00 +0100

    DEMO-849: api in progress (runs, results)

--- selenium-avaea-tests/src/avaea/tests/testrail.clj
+++ selenium-avaea-tests/src/avaea/tests/testrail.clj
@@ -5,11 +5,15 @@
             [clojure.string :as string]))
 
 (def testrail-config (-> (read-config) :testrail))
+
 (def browser-name (environ/env :clj-env))
+
 (def system-info
   {:name    (System/getProperty "os.name"),
    :version (System/getProperty "os.version"),
    :arch    (System/getProperty "os.arch")})
+
+(def headers {:content-type :json :accept :json})
 
 (defn- add-last-slash [url]
   (if (string/ends-with? url "/") url (str url "/")))
@@ -19,10 +23,62 @@
     (str (add-last-slash base-url) "index.php?/api/v2/" url)))
 
 (defn- send-get [url]
-  (client/get (build-url url) (:auth testrail-config)))
+  (:body (client/get (build-url url) (merge headers (:auth testrail-config)))))
 
-(defn- send-post [url params]
-  (client/post (build-url url) (merge (:auth testrail-config) {:body params})))
+(defn- send-post [url body]
+  (:body (client/post (build-url url) (merge headers (:auth testrail-config) {:form-params body}))))
+
+(defn get-status-id [status-id]
+  (if (keyword? status-id)
+    (case status-id
+      :passed 1
+      :blocked 2
+      :untested 3
+      :retest 4
+      :failed 5) status-id))
+
+(defn test-api [test-fn]
+  (try
+    (test-fn)
+    (catch Throwable e (-> e ex-data :body))))
+
+;; -------------------------------------------------------------------------------------
+
+(defn get-run
+  "Use the following API methods to request details about test runs and to create or modify test runs."
+  [run-id]
+  (send-get (str "get_run/" run-id)))
+
+(defn get-runs
+  "Returns a list of test runs for a project.
+   Only returns those test runs that are not part of a test plan
+   (please see get_plans/get_plan for this)."
+  [project-id]
+  (send-get (str "get_run/" project-id)))
+
+(defn add-run
+  "Creates a new test run."
+  [project-id {:keys [suite_id name description milestoneid assignedto_id include_all case_id] :as info}]
+  (send-post (str "add_run/" project-id) info))
+
+(defn update-run
+  "Updates an existing test run (partial updates are supported, i.e. you can submit and update specific fields only)."
+  [run-id {:keys [suite_id name description milestoneid assignedto_id include_all case_id] :as info}]
+  (send-post (str "update_run/" run-id) info))
+
+(defn close-run
+  "Closes an existing test run and archives its tests & results."
+  [run-id]
+  (send-post (str "close_run/" run-id) {}))
+
+(defn delete-run
+  "Deletes an existing test run."
+  [run-id]
+  (send-post (str "delete_run/" run-id) {}))
+
+(test-api #(delete-run 3))
+
+;; -------------------------------------------------------------------------------------
 
 (defn get-results
   "Returns a list of test results for a test."
@@ -31,20 +87,21 @@
 
 (defn get-results-for-case
   "Returns a list of test results for a test run and case combination."
-  [case-id]
-  (send-get (str "get_results_for_case/" case-id)))
+  [run-id case-id]
+  (send-get (str "get_results_for_case/" run-id "/" case-id)))
 
 (defn get-results-for-run
   "Returns a list of test results for a test run."
   [run-id]
   (send-get (str "get_results/" run-id)))
 
+
 (defn add-result
   "Adds a new test result, comment or assigns a test.
   It's recommended to use add_results instead if you
   plan to add results for multiple tests."
-  [test-id {:keys [status_id comment elapsed defects version custom_step_results] :as result}]
-  (send-post (str "add_result/" test-id) result))
+  [test-id {:keys [status_id comment elapsed defects version assigned_to custom_step_results] :as result}]
+  (send-post (str "add_result/" test-id) (merge result {:status_id (get-status-id status_id)})))
 
 (defn add-result-for-case
   "Adds a new test result, comment or assigns a test (for a test run and case combination).
@@ -58,6 +115,7 @@
   the differences between test cases and tests."
   [run-id case-id {:keys [status_id comment elapsed defects version custom_step_results] :as result}]
   (send-post (str "add_result_for_case/" run-id "/" case-id) result))
+
 
 (defn add-results
   "Adds one or more new test results, comments or assigns one or more tests.
