8601b74fce0a4aed93fdb9e69c677c2688e0a0ee
Eugene Tokarev <eugene.tokarev@gmail.com>
2017-01-23 17:43:07 +0700

    ONV-910 added ads on result page (2 & 3 cases)

--- src/assets/js/components/search/ResultList.jsx
+++ src/assets/js/components/search/ResultList.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import ResultItemContainer from './ResultItem.jsx';
+import Iframe from 'react-iframe';
 
-var ResultList = React.createClass({
-
+let ResultList = React.createClass({
   render: function() {
     return (
       <span>
       {(this.props.InitResultData.searchResultLength ?
       <div id="searchResultData" className="flights">
-        {this.props.InitResultData.searchResult.map(function(itinerary) {
+        {this.props.InitResultData.searchResult.map(function(itinerary, counter) {
           if (!itinerary.is_hidden) {
-            return <ResultItemContainer key={itinerary.id}
-                                        itinerary={itinerary} />
+            let result = null;
+            if (counter == 2) {
+              result = <span key="ad">
+                <div className="col-xs-12 itinerary">
+                  <Iframe id="c7aed39b" name="mc79eba9" url="/static/adds.html"
+                    frameborder="0" scrolling="no" width="100%" height="100%" position="relative"></Iframe>
+                </div>
+                <ResultItemContainer key={itinerary.id}
+                                     itinerary={itinerary} />
+              </span>;
+              return result;
+            } else {
+              return <ResultItemContainer key={itinerary.id}
+                                          itinerary={itinerary}/>;
+            }
           }
           return null;
         }.bind(this))}
