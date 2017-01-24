e7d91111f776d0126401a18374904577a25aa2da
Eugene Tokarev <eugene.tokarev@gmail.com>
2017-01-24 14:37:31 +0700

    ONV-910 fixed banner position on sorting and fitering

--- src/assets/js/components/search/ResultList.jsx
+++ src/assets/js/components/search/ResultList.jsx
@@ -4,29 +4,31 @@ import Iframe from 'react-iframe';
 
 let ResultList = React.createClass({
   render: function() {
+    let counter = 0;
+    let adWasShowed = false;
     return (
       <span>
       {(this.props.InitResultData.searchResultLength ?
       <div id="searchResultData" className="flights">
-        {this.props.InitResultData.searchResult.map(function(itinerary, counter) {
+        {this.props.InitResultData.searchResult.map(function(itinerary) {
+          let itin = null;
           if (!itinerary.is_hidden) {
-            let result = null;
-            if (!uaMobile && counter == 2) {
-              result = <span key="ad">
+            itin = <ResultItemContainer key={itinerary.id} itinerary={itinerary}/>;
+            counter++;
+          }
+
+          let ad = null;
+          if (!adWasShowed && !uaMobile && (counter == 2 || this.props.InitResultData.searchResultLength <= counter)) {
+            adWasShowed = true;
+            ad = <span key="ad">
+                {itin}
                 <div className="col-xs-12 itinerary">
                   <Iframe id="c7aed39b" name="mc79eba9" url="/static/adds.html"
-                    frameborder="0" scrolling="no" width="100%" height="100%" position="relative"></Iframe>
+                          frameborder="0" scrolling="no" width="100%" height="100%" position="relative"></Iframe>
                 </div>
-                <ResultItemContainer key={itinerary.id}
-                                     itinerary={itinerary} />
               </span>;
-              return result;
-            } else {
-              return <ResultItemContainer key={itinerary.id}
-                                          itinerary={itinerary}/>;
-            }
           }
-          return null;
+          return ad || itin;
         }.bind(this))}
       </div>
         : null
