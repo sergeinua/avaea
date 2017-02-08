6e094cf5d19f6d37e4e54539a6534da82bdfbf16
Dmitry Bratskiy <d.bratskyi@onvoya.com>
2017-02-08 14:07:48 +0000

    production.js edited online with Bitbucket

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -39,12 +39,21 @@ module.exports = {
     timestamp: true
   },
 
-  session: {
+/*  session: {
     host: 'db1',
     user: 'avaea',
     password: 'a1v2a3e4a5',
     database: 'avaea'
-  },
+  },*/
+  session:{
+  adapter: 'redis',
+  host: 'rediscache.5suwap.0001.usw2.cache.amazonaws.com',
+  port: 6379,
+  ttl: 600,
+  db: 0,
+  pass: '',
+  prefix: 'sess:'
+}
   connections: {
    /* memcacheConf: {
     host: 'cache1.5suwap.cfg.usw2.cache.amazonaws.com',
