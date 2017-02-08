577ffc82a52a4bb91549d68426809e104999af07
Dmitry Bratskiy <d.bratskyi@onvoya.com>
2017-02-08 13:17:56 +0000

    production.js edited online with Bitbucket

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -46,11 +46,21 @@ module.exports = {
     database: 'avaea'
   },
   connections: {
-    memcacheConf: {
+   /* memcacheConf: {
     host: 'cache1.5suwap.cfg.usw2.cache.amazonaws.com',
     port: '11211',
     exptime: 60*30 // 30 minutes
+  }, */
+  redisConf: {
+    host: 'rediscache.5suwap.0001.usw2.cache.amazonaws.com',
+    port: 6379,
+    ttl: 600,
+    db: 0,
+    pass: '',
+    prefix: 'cache:',
+    exptime: 60*30 // 30 minutes
   },
+
 },
   hookTimeout: 60000,
   segmentio_key: 'oFl2tXWI9epbfKgbTEvJsuBDfdE1h1Q2',
