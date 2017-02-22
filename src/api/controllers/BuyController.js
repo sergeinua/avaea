18eb6991cb6ba9d46f5f80c9fc8b207a492650ac
Igor A <igorya.inscriptio@gmail.com>
2017-02-22 18:29:23 +0200

    Merge2 branches 'ONV-1010' and 'master' of https://bitbucket.org/avaea_eng/avaea into ONV-1010
    # Conflicts:
    #	src/api/controllers/BuyController.js

--- src/api/controllers/BuyController.js
+++ src/api/controllers/BuyController.js
@@ -175,7 +175,7 @@ module.exports = {
 
         req.session.time_log = [];
         reqParamsApi.session = reqParams.session = req.session;
-        reqParamsApi.ip = req.ip;
+        reqParamsApi.ip = utils.getIP(req);
 
         return global[booking_itinerary.service].flightBooking(Search.getCurrentSearchGuid() +'-'+ booking_itinerary.service, reqParamsApi, parseFlightBooking);
       })
