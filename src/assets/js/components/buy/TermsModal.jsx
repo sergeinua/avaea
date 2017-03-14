d13b0a976250a12fb4ed33bd209514e97e77c31d
Eugene Tokarev <eugene.tokarev@gmail.com>
2017-03-14 16:33:13 +0700

    ONV-1129 added modal terms page on link click (no styles)

--- src/assets/js/components/buy/TermsModal.jsx
+++ src/assets/js/components/buy/TermsModal.jsx
@@ -1,44 +1,36 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
 import StaticTerms from '../static/terms/Terms.jsx';
 
 let TermsModal = React.createClass({
-	
-	componentDidMount: function () {
-		$('.terms-modal').modal({
-	    backdrop: 'static',
-	    keyboard: false
-	  });
-	},
-	
-	closeModal: function () {
+
+  closeModal: function () {
     $('.terms-modal').modal('hide');
-	},
+  },
 
   render: function () {
 
     return (
-    		<div className="modal terms-modal" id="TermsModal" role="dialog">
-	        <div className="modal-dialog">
-	          <div className="modal-content">
-	          
-		          <div className="title-bar">
-			          <div className="close-x" onClick={this.closeModal()}></div>
-			          <div className="title">Terms</div>
-		          </div>
-	          
-	            <div className="modal-body">
-	
-		              <div className="terms-holder">
-		               
-		              	<StaticTerms/>
-		          
-		              </div>
-	              
-	            </div>
-	          </div>
-	        </div>
-	      </div>
+        <div className="modal terms-modal" id="TermsModal" role="dialog">
+          <div className="modal-dialog">
+            <div className="modal-content">
+
+              <div className="title-bar">
+                <div className="close-x" onClick={this.closeModal}></div>
+                <div className="title">Terms</div>
+              </div>
+
+              <div className="modal-body">
+
+                  <div className="terms-holder">
+
+                    <StaticTerms/>
+
+                  </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
     );
   }
 
